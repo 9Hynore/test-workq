41e9a1e03f744c25df254833f7d0e892bb82f7a7
Hynore <68786826+9Hynore@users.noreply.github.com>
2023-03-11 19:58:23 +0600

    new

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const menuBtn = document.querySelector('.icon__menu');
 const menu = document.querySelector('.header__menu');
-
+const body = document.querySelector('body');
 
 menuBtn.addEventListener('click', () =>{
 	menu.classList.toggle('active');
